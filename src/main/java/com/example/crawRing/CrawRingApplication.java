feea95eca85a9f24fb2676974943fcfc794c374b
Kangboom <jkl6147@naver.com>
2024-04-04 00:52:22 +0900

    Initial commit

---
+++ src/main/java/com/example/crawRing/CrawRingApplication.java
@@ -0,0 +1,13 @@
+package com.example.crawRing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawRingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawRingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crawRing/CrawRingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crawRing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawRingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
